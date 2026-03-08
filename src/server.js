fbfa1a8c8c2f293735965018973dc0f25ac06f72
alejozd <alejozd79@gmail.com>
2026-03-08 01:14:45 -0500

    oriegen de la misma ip del servidor

--- src/server.js
+++ src/server.js
@@ -9,7 +9,13 @@ import { ensureAdminUser } from "./seed/adminSeed.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://192.168.1.50:8081",
+    credentials: true,
+  }),
+);
+
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
